9ffe7dd2fccd60ab9151106a2a89bde424abe653
moura1001 <gmouraneto01@gmail.com>
2022-01-01 12:20:01 -0300

    commit com adição da ferramenta de análise de cobertura de testes Cloverage

--- project.clj
+++ project.clj
@@ -4,11 +4,12 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
-                 [ring/ring-defaults "0.3.2"]
-                 [midje "1.10.5"]]
+                 [ring/ring-defaults "0.3.2"]]
   :plugins [[lein-ring "0.12.5"]
-            [lein-midje "3.2.1"]]
+            [lein-midje "3.2.1"]
+            [lein-cloverage "1.2.2"]]
   :ring {:handler controle-financeiro.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.2"]]}})
+                        [ring/ring-mock "0.3.2"]
+                        [midje "1.10.5"]]}})
