40ca8fecb56bee9403a82cb8ca1c42d710157d65
moura1001 <gmouraneto01@gmail.com>
2022-01-04 10:40:56 -0300

    commit com correção nos testes unitários da rota /saldo para o correto consumo do JSON

--- test/unitarios/controle_financeiro/saldo_test.clj
+++ test/unitarios/controle_financeiro/saldo_test.clj
@@ -1,14 +1,19 @@
 (ns controle-financeiro.saldo-test
   (:require [controle-financeiro.handler :refer [app]]
             [midje.sweet :refer :all]
-            [ring.mock.request :as mock]))
+            [ring.mock.request :as mock]
+            [cheshire.core :as json]))
   
 (facts "Saldo inicial deve ser 0"
+  (against-background
+    (json/generate-string {:saldo 0}) => "{\"saldo\":0}"
+  )
   (let [response (app (mock/request :get "/saldo"))]
     (fact "O status da resposta é 200"
       (:status response) => 200)
-    (fact "O texto do corpo é '0'"
-      (:body response) => "0")
+      
+    (fact "O texto do corpo é um JSON cuja chave é saldo e o valor é 0"
+      (:body response) => "{\"saldo\":0}")
   )
 )
   
