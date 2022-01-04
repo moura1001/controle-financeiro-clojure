61c2051c94b0f67a98ac6f2a77c9db517fb85228
moura1001 <gmouraneto01@gmail.com>
2022-01-04 10:16:59 -0300

    commit com modificação na resposta retornada pela rota /saldo, agora produzindo um JSON

--- src/controle_financeiro/handler.clj
+++ src/controle_financeiro/handler.clj
@@ -1,11 +1,12 @@
 (ns controle-financeiro.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [cheshire.core :as json]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
-  (GET "/saldo" [] "0")
+  (GET "/saldo" [] (json/generate-string {:saldo 0}))
   (route/not-found "Not Found"))
 
 (def app
