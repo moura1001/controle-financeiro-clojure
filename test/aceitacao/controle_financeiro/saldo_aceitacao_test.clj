1421ea683c5149cad2066dc2c1a34dbed69332e6
moura1001 <gmouraneto01@gmail.com>
2022-01-05 10:38:07 -0300

    commit com primeira fase de uma nova iteração do TDD, visando permitir a criação de transações de despesa ou receita

--- test/aceitacao/controle_financeiro/saldo_aceitacao_test.clj
+++ test/aceitacao/controle_financeiro/saldo_aceitacao_test.clj
@@ -1,7 +1,9 @@
 (ns controle-financeiro.saldo-aceitacao-test
   (:require [midje.sweet :refer :all]
             [ring.mock.request :as mock]
-            [controle-financeiro.auxiliares :refer :all]))
+            [controle-financeiro.auxiliares :refer :all]
+            [cheshire.core :as json]
+            [clj-http.client :as http]))
 
 (against-background
   [
@@ -13,5 +15,17 @@
     (parse-string-producing-keywords-as-keys
       (conteudo "/saldo")
     ) => {:saldo 0}
+  )
+  
+  (fact "O saldo é 10 quando a única transação é uma receita de 10"
+    :aceitacao
+    (http/post
+      (endereco-para "/transacoes")
+      {:body (json/generate-string {:valor 10 :tipo "receita"})}
+    )
+    
+    (parse-string-producing-keywords-as-keys
+      (conteudo "/saldo")
+    ) => {:saldo 10}
   )
 )
