e304dba9c138b5910a0bd91afaeb8aa374aeeb9d
moura1001 <gmouraneto01@gmail.com>
2022-01-06 10:54:44 -0300

    commit com testes unitários para a função que calcula o saldo

--- test/unitarios/controle_financeiro/db_test.clj
+++ test/unitarios/controle_financeiro/db_test.clj
@@ -20,4 +20,36 @@
     (count (transacoes)) => 1
   )
 )
+
+(facts "Calcula o saldo dada uma coleção de transações"
+  (against-background
+    [
+      (before :facts (limpar-colecao))
+    ]
+  )
+  
+  (fact "Saldo é positivo quando só tem receitas"
+    (registrar {:valor 1 :tipo "receita"})
+    (registrar {:valor 2 :tipo "receita"})
+    (registrar {:valor 4 :tipo "receita"})
+    (registrar {:valor 8 :tipo "receita"})
+    (saldo) => 15
+  )
+  
+  (fact "Saldo é negativo quando só tem despesas"
+    (registrar {:valor 16 :tipo "despesa"})
+    (registrar {:valor 32 :tipo "despesa"})
+    (registrar {:valor 64 :tipo "despesa"})
+    (registrar {:valor 128 :tipo "despesa"})
+    (saldo) => -240
+  )
+  
+  (fact "Saldo é a soma das receitas menos a soma das despesas"
+    (registrar {:valor 1 :tipo "despesa"})
+    (registrar {:valor 16 :tipo "receita"})
+    (registrar {:valor 8 :tipo "despesa"})
+    (registrar {:valor 128 :tipo "receita"})
+    (saldo) => 135
+  )
+)
   
