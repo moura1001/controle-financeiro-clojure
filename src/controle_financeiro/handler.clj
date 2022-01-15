c30681671a262619560055d7d204c4bd0913c07e
moura1001 <gmouraneto01@gmail.com>
2022-01-15 10:11:25 -0300

    commit com concretização dos testes unitários para a rota /transacoes, visando a filtragem de transações por rótulos

--- src/controle_financeiro/handler.clj
+++ src/controle_financeiro/handler.clj
@@ -27,8 +27,16 @@
       (como-json {:mensagem "Requisição inválida"} 422)
     )
   )
-  (GET "/transacoes" []
-    (como-json {:transacoes (db/transacoes)})
+  (GET "/transacoes" {filtros :params}
+    (como-json
+      {
+        :transacoes
+        (if (empty? filtros)
+          (db/transacoes)
+          (db/transacoes-com-filtro filtros)
+        )
+      }
+    )
   )
   (GET "/despesas" []
     (como-json {:transacoes (db/transacoes-do-tipo "despesa")})
